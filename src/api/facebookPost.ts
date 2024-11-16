5f6e8553e066f4df5dfe071fe56e11aacff738c0
milan mishra <milan1mishra2@gmail.com>
2024-11-17 02:51:53 +0545

    kto finally

--- src/api/facebookPost.ts
+++ src/api/facebookPost.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 export const postToFacebook = async (message: string) => {
-  const accessToken =
-    "EAAOPUy8T8dsBOZBZADw41fCWZBw8GBuuM4f66tkm2MhYxOO86rSa36Yvf0yToZC1lk8K893qgZAKEAlmr4moj2LgqjXgIjrD5Soo42ZCS45qgHOiFHZBa0ZAGJ4ptPAfO4QgYv7Up3fgAbZAHPOSMdZBGqlz2HIpZC3IZAJAIO6xKMy7L9jJ5RM51QyBQGQ4i2xEBSwh";
   const pageId = "488062707724033";
 
+  const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
   const formData = new FormData();
   formData.append("message", message);
-  formData.append("access_token", accessToken);
+  formData.append("access_token", API_KEY);
   const response = await axios.post(
     `https://graph.facebook.com/v12.0/${pageId}/feed`,
     formData
