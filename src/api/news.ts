30a90785512166469e25acf435413573203c457a
milan mishra <milan1mishra2@gmail.com>
2024-11-17 00:23:28 +0545

    weather and news api added

--- src/api/news.ts
+++ src/api/news.ts
@@ -6,3 +6,8 @@ export const getAll = async () => {
     const response = await axios.get(url);
     return response.data;
 };
+
+//api key
+// NEXT_PUBLIC_NEWS_API_KEY=ad4b453afd7449e6a624fa0fd1f329f3
+
+// NEXT_PUBLIC_YOUTUBE_API_KEY=AIzaSyA3hTg7AD4fQomSwk7K57ku6Skkl5d6Iyw
