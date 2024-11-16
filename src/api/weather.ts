506344fde9fc3ea4d7c1cde7a40011f09e253208
milan mishra <milan1mishra2@gmail.com>
2024-11-17 00:22:59 +0545

    weather and news api added

---
+++ src/api/weather.ts
@@ -0,0 +1,18 @@
+import config from "@/config"
+import axios from "axios";
+
+interface GetWeatherDataParams {
+    latitude: number;
+    longitude: number;
+}
+
+export const getAll = async ({latitude, longitude}: GetWeatherDataParams) => {
+    const url = `${config.OPEN_METEO_API_URL}?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,rain,relative_humidity_2m,wind_speed_10m,cloudcover`;
+    const response = await axios.get(url);
+    return response.data;
+};
+
+//api key
+
+// NEXT_PUBLIC_OPEN_WEATHER_API_URL=https://api.openweathermap.org/data/2.5/weather
+// NEXT_PUBLIC_OPEN_WEATHER_API_KEY=19c5ce24fadd06394909e2842d8c5aca
