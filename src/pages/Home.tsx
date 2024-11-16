f5b71cacf2bec0d4af1f525a8c628795d6a32e7f
milan mishra <milan1mishra2@gmail.com>
2024-11-16 21:08:14 +0545

    gall map

---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import MapComponent from "../componenets/GalliMap";
+import Search from "../componenets/Search";
+
+
+
+const Home = () => {
+  const [searchedLocation, setSearchedLocation] = useState<{ lat: number; lng: number } | null>(
+    null
+  );
+
+  return (
+    <div className="min-h-screen w-full relative">
+      {/* Map Component */}
+      <MapComponent />
+
+      Search Component
+      <Search onLocationSelect={setSearchedLocation} />
+    </div>
+  );
+};
+
+export default Home;
